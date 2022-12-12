d3b293de755f26089c5b654b30ca7ca7d1bac74f
TGKElit <tgkelit@gmail.com>
2022-12-12 21:03:47 +0100

    added buttons and hid an image

--- main.js
+++ main.js
@@ -4,21 +4,57 @@ let intervalId;
 let oldX;
 let oldY;
 let mousedown = 0;
+let nodeNumber = 36;
+let oldNodes = [];
+let pictureId;
 
-for (i = -15; i < 15; i++) {
-    for (j = -15; j < 15; j++) {
-        if(Math.random() < 0.7) {
-            const x = (i * 100) + Math.random() * 50;
-            const y = (j * 100) + Math.random() * 50;
-            newNode = document.createElement("div");
-            newNode.setAttribute("id", "node" + indexCounter);
-            newNode.classList.add("node");
-            newNode.setAttribute("style", "top: " + y + "px; left: " + x + "px;");
-            document.body.appendChild(newNode);
-            nodes[indexCounter] = [x, y];
-            indexCounter++;
+nodeGenerator();
+
+document.getElementById("few").addEventListener("click", function() {
+    nodeNumber = 400;
+    nodeGenerator();
+});
+document.getElementById("medium").addEventListener("click", function() {
+    nodeNumber = 900;
+    nodeGenerator();
+});
+document.getElementById("many").addEventListener("click", function() {
+    nodeNumber = 2500;
+    nodeGenerator();
+});
+
+
+function nodeGenerator() {
+    oldNodes = document.getElementsByClassName('node');
+    while(oldNodes[0]) {
+        oldNodes[0].parentNode.removeChild(oldNodes[0]);
+    }
+    nodes = [];
+    for (i = -Math.sqrt(nodeNumber)/2; i < Math.sqrt(nodeNumber)/2; i++) {
+        for (j = -Math.sqrt(nodeNumber)/2; j < Math.sqrt(nodeNumber)/2; j++) {
+            if(Math.random() < 0.7) {
+              const x = (i * 100) + Math.random() * 50;
+              const y = (j * 100) + Math.random() * 50;
+              newNode = document.createElement("div");
+              newNode.setAttribute("id", "node" + indexCounter);
+              newNode.classList.add("node");
+              newNode.setAttribute("style", "top: " + y + "px; left: " + x + "px;");
+              newNode.setAttribute("onclick", "show(this)");
+              document.body.appendChild(newNode);
+              nodes[indexCounter] = [x, y];
+              indexCounter++;
+            }
         }
     }
+    pictureId = Math.floor(Math.random()*nodes.length);
+    pictureNode = document.getElementById("node" + pictureId);
+    pictureNode.classList.add("picture-node");
+    picture = pictureNode.appendChild(document.createElement("img"));
+    picture.setAttribute("src", "/Rick.png");
+}
+
+function show(node) {
+    node.classList.toggle("shown");
 }
 
 document.addEventListener("pointermove", function(e) {
@@ -62,13 +98,14 @@ moveFocus = function (e) {
         currentNode.style.zIndex = Math.round(size);
         currentNode.style.left = node[0] - size/2 + Math.pow(size, 2) * (xDistance) / 10000 + "px";
         currentNode.style.top = node[1] - size/2 + Math.pow(size, 2) * (yDistance) / 10000 + "px";
+
+        //currentNode.innerHTML = size;
     });
 }
 
 dragScreen = function (e) {
     e.preventDefault();
     if(mousedown == 1){
-        console.log(oldX + " | " + oldY);
         nodes.forEach(node => {
             node[0] += e.x-oldX;
             node[1] += e.y-oldY;
@@ -91,3 +128,4 @@ ungrab = function (e) {
     document.body.style.cursor = "";
     mousedown = 0;
 }
+
